d37c929f88655de7ce5199e7da9d338a88085873
Leif Janzik <leif.janzik@gmail.com>
2017-12-14 15:28:15 +0100

    fixed request for available funds and investments
    switched from HTML+Xpath to json processing as the server response was json
    minor tweaks to from-since calculation to entries for "today" are requested

--- Mintos.lua
+++ Mintos.lua
@@ -1,5 +1,5 @@
 WebBanking {
-    version = 1.1,
+    version = 1.2,
     url = "https://www.mintos.com/",
     services = { "Mintos Account" }
 }
@@ -11,7 +11,7 @@ function SupportsBank (protocol, bankCode)
 end
 
 function InitializeSession (protocol, bankCode, username, username2, password, username3)
-    connection = Connection() 
+    connection = Connection()
     local html = HTML(connection:get(url))
     local csrfToken = html:xpath("//*[@id='login-form']/input[@name='_csrf_token']"):val()
 
@@ -50,7 +50,7 @@ function ListAccounts (knownAccounts)
 end
 
 function RefreshAccount (account, since)
-    local datePattern = "(%d+)%.(%d+)%.(%d+).*%s(%d+):(%d+)"
+    local datePattern = "(%d+)%.(%d+)%.(%d+)"
 
     if string.sub(account.accountNumber, -1) == '1' then
         local html = HTML(connection:get("https://www.mintos.com/en/overview/"))
@@ -58,36 +58,44 @@ function RefreshAccount (account, since)
         local balance = tonumber(string.match(html:xpath('//*[@class="overview-box"][1]/div/table/tr[1]/td[2]'):text(), ".*%s(%d+%.%d+).*"))
 
         local transactions = {}
+		local oneDay = (24*60*60)
         local oneMonth = 4*7*24*60*60
 
+
         local toDate = since + oneMonth
-        while toDate < os.time() do
 
-            print("Getting transactions from " .. os.date("%d.%m.%Y", since) .. " to " .. os.date("%d.%m.%Y", toDate))
+        while toDate <= os.time() do
+            print("Getting transactions from " .. os.date("%d.%m.%Y", since) .. " to " .. os.date("%d.%m.%Y", toDate+oneDay))
 
-            local list = HTML(connection:request("POST",
+            local list = JSON(connection:request("POST",
             "https://www.mintos.com/en/account-statement/list",
-            "account_statement_filter[fromDate]=" .. os.date("%d.%m.%Y", since) .. "&account_statement_filter[toDate]=" .. os.date("%d.%m.%Y", toDate) .. "",
-            "application/x-www-form-urlencoded; charset=UTF-8"))
+            "account_statement_filter[fromDate]=" .. os.date("%d.%m.%Y", since) .. "&account_statement_filter[toDate]=" .. os.date("%d.%m.%Y", toDate+oneDay) .. "",
+            "application/x-www-form-urlencoded; charset=UTF-8; Accept:text/html")):dictionary()
 
-            list:xpath('//*[@id="overview-details"]/table/tbody/tr[not(@class)]'):each(function (index, element)
-                local dateString = element:xpath('.//*[@class="m-transaction-date"]'):attr('title')
-                local day, month, year, hour, min = dateString:match(datePattern)
+			if list["data"]["summary"]["total"]>0 then
+			for i, element in ipairs(list["data"]["summary"]["accountStatements"]) do
+			
+			   local dateString = element["date"]
+               local day, month, year = dateString:match(datePattern)
 
-                local purpose = element:xpath('.//*[@class="m-transaction-details"]'):text()
+               local purpose = element["details"]
 
-                local amount = element:xpath('.//*[contains(concat(" ", normalize-space(@class), " "), " m-transaction-amount ")]'):text()
+               local amount = element["turnover"]
 
-                local transaction = {
-                    bookingDate = os.time({day=day,month=month,year=year,hour=hour,min=min}),
-                    purpose = purpose,
-                    amount = tonumber(amount)
-                }
-                table.insert(transactions, transaction)
-            end)
-            since = toDate - 24*60*60
+               local transaction = {
+                   bookingDate = os.time({day=day,month=month,year=year,hour=0,min=0}),
+                   purpose = StripHtml(purpose),
+                   amount = tonumber(amount)
+               }
+               table.insert(transactions, transaction)
+			end
+			end
+
+            since = toDate + oneDay
             toDate = toDate + oneMonth
         end
+
+
 
         return {
             balance = balance,
@@ -97,40 +105,66 @@ function RefreshAccount (account, since)
 
         local page = 1
         local total = 1000
-        local showing = 0
 
         local securities = {}
 
-        while showing < total do
-            local list = HTML(connection:request("POST",
+        while page < total do
+            local list = JSON(connection:request("POST",
             "https://www.mintos.com/en/my-investments/list",
             "statuses%5B%5D=256&statuses%5B%5D=512&statuses%5B%5D=1024&statuses%5B%5D=2048&statuses%5B%5D=8192&statuses%5B%5D=16384&max_results=100&page=" .. page,
-            "application/x-www-form-urlencoded; charset=UTF-8"))
+            "application/x-www-form-urlencoded; charset=UTF-8")):dictionary()
 
-
-            list:xpath('//*[@id="investor-investments-table"]/tbody/tr[not(contains(@class, "total-row"))]'):each(function (index, element)
-                local dateOfPurchaseString = element:xpath('.//*[contains(concat(" ", normalize-space(@class), " "), " m-loan-issued ")]'):attr('title')
+            for j, element in ipairs(list["data"]["result"]["investments"]) do
+                local dateOfPurchaseString = element["createdAt"]
                 local day, month, year, hour, min = dateOfPurchaseString:match(datePattern)
 
-                local name = element:xpath('.//*[contains(concat(" ", normalize-space(@class), " "), " m-loan-id ")]'):text()
-                local price = string.match(element:xpath('.//*[@data-m-label="Outstanding Principal"]'):text(), ".*%s(%d+%.%d+).*")
+                local name = element["loan"]["identifier"]
+                local price = element["amount"]
+				local type = element["loan"]["type"]
+				local groupIdentifier = element["loan"]["groupIdentifier"]
 
                 local security = {
                     dateOfPurchase = os.time({day=day,month=month,year=year,hour=hour,min=min}),
-                    name = name,
+                    name = type .. " - " .. groupIdentifier .. " - " .. name,
                     currency = 'EUR',
                     amount = tonumber(price)
                 }
                 table.insert(securities, security)
-            end)
+            end
 
-            showing = list:xpath('//*[@id="result-status"]/span[@class="to"]'):text()
-            total = list:xpath('//*[@id="result-status"]/span[@class="total"]'):text()
-            page = page + 1
+            total = list["data"]["result"]["totalPages"]
+            page = list["data"]["result"]["pagination"]["currentPage"] + 1
         end
 
         return {securities = securities}
     end
+end
+
+function StripHtml (t)
+	local cleaner = {
+		{ "&amp;", "&" }, -- decode ampersands
+		{ "&#151;", "-" }, -- em dash
+		{ "&#146;", "'" }, -- right single quote
+		{ "&#147;", "\"" }, -- left double quote
+		{ "&#148;", "\"" }, -- right double quote
+		{ "&#150;", "-" }, -- en dash
+		{ "&#160;", " " }, -- non-breaking space
+		{ "<br ?/?>", "\n" }, -- all <br> tags whether terminated or not (<br> <br/> <br />) become new lines
+		{ "</p>", "\n" }, -- ends of paragraphs become new lines
+		{ "(%b<>)", "" }, -- all other html elements are completely removed (must be done last)
+		{ "\r", "\n" }, -- return carriage become new lines
+		{ "[\n\n]+", "\n" }, -- reduce all multiple new lines with a single new line
+		{ "^\n*", "" }, -- trim new lines from the start...
+		{ "\n*$", "" }, -- ... and end
+	}
+
+	-- clean html from the string
+	for i=1, #cleaner do
+		local cleans = cleaner[i]
+		t = string.gsub( t, cleans[1], cleans[2] )
+	end
+
+	return t
 end
 
 function EndSession ()
